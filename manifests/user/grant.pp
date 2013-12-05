f7924576b761f79b1ec9e4012683672e82608365
Michael Schaefermeyer <michael.schaefermeyer@gmail.com>
2013-12-05 13:49:48 +0100

    fix grant

--- manifests/user/grant.pp
+++ manifests/user/grant.pp
@@ -5,18 +5,19 @@ define rabbitmq::user::grant(
 ){
   require rabbitmq
   require rabbitmq::config
+
   if $ensure == 'present' {
     exec { "grant user ${name} right ${permissions} on vhost ${vhost}":
       command => "${rabbitmq::config::rabbitmqctl} set_permissions -p ${vhost} ${user} ${permissions}",
       unless  => "${rabbitmq::config::rabbitmqctl} list_permissions -p ${vhost} ${user}",
-      require => Service['dev-rabbitmq']
+      require => Service['dev.rabbitmq']
     }
   }
   elsif $ensure == 'absent' {
     exec { "revoke permissions from user ${name} on vhost ${vhost}":
       command => "${rabbitmq::config::rabbitmqctl} clear_permissions -p ${vhost} ${user}",
       onlyif  => "${rabbitmq::config::rabbitmqctl} list_permissions -p ${vhost} ${user}",
-      require => Service['dev-rabbitmq']
+      require => Service['dev.rabbitmq']
     }
   }
 }
