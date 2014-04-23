79a9fe69897099f43911987a3ce07757f8f18f96
Peter Gumeson <gumeson@gmail.com>
2014-04-23 14:56:16 -0700

    Fix issues with user::grant module

--- manifests/user/grant.pp
+++ manifests/user/grant.pp
@@ -1,23 +1,23 @@
 define rabbitmq::user::grant(
   $user,
   $vhost,
-  $ensure      = 'present',
-  $permissions = '".*" ".*" ".*"'
+  $ensure      = "present",
+  $permissions = "'.*' '.*' '.*'"
 ){
   require rabbitmq
   require rabbitmq::config
 
   if $ensure == 'present' {
-    exec { "grant user ${name} right ${permissions} on vhost ${vhost}":
+    exec { "grant user ${user} right ${permissions} on vhost ${vhost}":
       command => "${rabbitmq::config::rabbitmqctl} set_permissions -p ${vhost} ${user} ${permissions}",
-      unless  => "${rabbitmq::config::rabbitmqctl} list_permissions -p ${vhost} ${user}",
+      unless  => "${rabbitmq::config::rabbitmqctl} list_user_permissions ${user} | grep ${vhost}",
       require => Exec['wait-for-rabbitmq']
     }
   }
   elsif $ensure == 'absent' {
-    exec { "revoke permissions from user ${name} on vhost ${vhost}":
+    exec { "revoke permissions from user ${user} on vhost ${vhost}":
       command => "${rabbitmq::config::rabbitmqctl} clear_permissions -p ${vhost} ${user}",
-      onlyif  => "${rabbitmq::config::rabbitmqctl} list_permissions -p ${vhost} ${user}",
+      onlyif  => "${rabbitmq::config::rabbitmqctl} list_user_permissions ${user} | grep ${vhost}",
       require => Exec['wait-for-rabbitmq']
     }
   }
